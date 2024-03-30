339b1fd74f2e9e1ab526c1f53b4cad5f78aa31ec
rmyz <romeuhd@gmail.com>
2024-03-31 00:22:43 +0100

    add rmyz

--- keyboards/kbdfans/kbd67/mkiirgb_iso/rules.mk
+++ keyboards/kbdfans/kbd67/mkiirgb_iso/rules.mk
@@ -12,4 +12,5 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = yes	
+TAP_DANCE_ENABLE = yes
